35ffb5fd8d308f92045827ff229d7d6d18e9555b
KentNyiLynn <kentnyilynn@proton.me>
2025-02-17 13:34:43 +0700

    set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup-tests.ts'
+  }
 })
